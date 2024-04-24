888ad0d5d32c224a9a977b57f61bfd86701aad4c
WanjikuN <cikunjoroge4@gmail.com>
2024-04-24 16:23:42 +0300

    Add tables

---
+++ models.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+userAlbums_association = db.Table(
+    'user_albums',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('album_id', db.Integer, db.ForeignKey('albums.id'))
+)
+class User(db.Model, SerializerMixin):
+    __tablename__ ='users'
+
+    serialize_rules = ('-albums',)
+
+    #columns
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.column(db.String) 
+    username = db.column(db. String, unique=True)
+    email = db.column(db.String, unique=True)
+
+    #relationships
+    albums = db.relationship('Album', secondary = userAlbums_association, back_populates='users')
+
+
+class Album(db.Model, SerializerMixin):
+    __tablename__ ='albums'
+    serialize_rules = ('-users',)
+
+    #columns
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.column(db.String) 
+    users_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # Relationships
+    users = db.relationship('User', secondary=userAlbums_association, back_populates='albums')
+
+
+class Photo(db.Model, SerializerMixin):
+    __tablename__ ='photos'
+
+    serialize_rules = ('-albums.photos',)
+
+    #columns
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.column(db.String) 
+    image_url = db.column(db.String)
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"))
+    
+    # relationships
+    albums = db.relationship('Album', backref='photos')
+
+
