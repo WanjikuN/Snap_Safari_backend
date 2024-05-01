6a322ee840bdadfbf60a02f1d10b73aa7f35940f
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 15:25:34 +0300

    Update Cors

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ db.init_app(app)
 api = Api(app, version='1.0', title='SnapSafari API',
           description='An API for managing users, albums, and photos')
 
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-CORS(app, resources={r"/*": {"origins": "*, http://localhost:36859"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:36859"]}}, supports_credentials=True)
 
 user_fields = api.model('User', {
     'id': fields.Integer,
